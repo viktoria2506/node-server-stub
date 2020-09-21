223686fe1e291d5c00e121d3497d58a1ef56a009
Victoriya Chernookaya (DevExpress) <chernookaya.victoriya@corp.devexpress.com>
2020-09-21 14:58:58 +0300

    static server

---
+++ config.js
@@ -0,0 +1,6 @@
+const host   = process.env.host || 'localhost';
+const port   = process.env.port || 1337;
+const config = { host, port };
+
+export { host, port, config };
+//export default config;
--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-// TODO: Start prototyping here
+import { config } from './config.js';
+import express from 'express';
+
+const app = express();
+
+app.use(express.static('./resources/pages'));
+
+app.listen(config.port, () => {
+    console.log(`Example app listening at http://localhost:${config.port}`);
+});
+
