3c31cab03bfcd4341752ed5b42f535b257665790
Viktoriya Chernookaya <32044161+viktoria2506@users.noreply.github.com>
2020-09-30 16:50:24 +0300

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,7 @@
-const gulp = require('gulp');
-const run    = require('gulp-run');
-const eslint = require('gulp-eslint');
-const testcafe = require('gulp-testcafe');
-const babel = require('gulp-babel');
+const gulp     = require('gulp');
+const run      = require('gulp-run');
+const eslint   = require('gulp-eslint');
+const babel    = require('gulp-babel');
 
 gulp.task('lint', () => {
     return gulp
@@ -19,20 +18,14 @@ gulp.task('unit-test', () => {
     return run('mocha --require babel-register test/unit/*-test.js').exec();
 });
 
-gulp.task('run-server', () => {
-    require('./dist/app');
-});
-
-gulp.task('functional-test', () => {
-    return gulp.src('test/functional/**/*.js')
-        .pipe(testcafe({ browsers: ['chrome'], reporter: { name: 'spec' } }));
-});
-
-gulp.task('check', gulp.series('lint', 'unit-test', 'functional-test'));
-
 gulp.task('build', () => {
     return gulp.src('src/*.js')
         .pipe(babel())
         .pipe(gulp.dest('dist'));
 });
 
+gulp.task('run-server', () => {
+    require('./dist/app');
+});
+
+gulp.task('start', gulp.series('build', 'run-server'));
